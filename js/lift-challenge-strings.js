8f9d4c89c52f3ec2590915b6e3dbba2f1acbd6e0
samreid <reids@colorado.edu>
2020-02-27 12:08:00 -0700

    Add images and strings, see https://github.com/phetsims/chipper/issues/875

---
+++ js/lift-challenge-strings.js
@@ -0,0 +1,14 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+
+import getStringModule from '../../chipper/js/getStringModule.js';
+import liftChallenge from './liftChallenge.js';
+
+const liftChallengeStrings = getStringModule( 'LIFT_CHALLENGE' );
+
+liftChallenge.register( 'liftChallengeStrings', liftChallengeStrings );
+
+export default liftChallengeStrings;
